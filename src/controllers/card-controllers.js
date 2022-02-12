2ae6b15395c8e419a598b34a31e21e757cad49ea
Joan <guigomene15@gmail.com>
2022-02-12 14:31:09 -0300

    POST GET DELETE PUT (Cant update)

--- src/controllers/card-controllers.js
+++ src/controllers/card-controllers.js
@@ -1,76 +1,76 @@
 const neDB = require('../configurations/database')
 const api = {}
 
-api.save = (request, response) => {
-    const canonical = request.body
+api.save = (req, res) => {
+    const canonical = req.body
     neDB.insert(canonical, (exception, cards) => {
         if(exception) { // undefined ele é igual a false
             const setence = 'CANT SAVE'
             console.error(setence, exception)
 
-            response.status(exception.status | 400)
-            response.json({ 'mensagem': setence })
+            res.status(exception.status | 404)
+            res.json({ 'mensagem': setence })
         }
         console.log('CARD SAVED', cards)
-        response.status(201)
-        response.json(cards)
+        res.status(201)
+        res.json(cards)
     })
 }
 
-api.findAll = (request, response) => {
+api.findAll = (req, res) => {
     neDB.find({}).sort({ name: 1 }).exec((exception, cards) => {
         if (exception) {
           const setence = "CANT LIST";
           console.error(setence, exception);
   
-          response.status(exception.status | 400);
-          response.json({ mensagem: setence });
+          res.status(exception.status | 404);
+          res.json({ mensagem: setence });
         }
         console.log("LISTED CARDS", cards);
-        response.json(cards);
+        res.json(cards);
       });
 };
 
-api.findOne = (request, response) => {
-    neDB.findOne({ _id: request.params.id }).sort({ name: 1 }).exec((exception, cards) => {
+api.findOne = (req, res) => {
+    neDB.findOne({ _id: req.params.id }).sort({ name: 1 }).exec((exception, cards) => {
         if (exception) {
           const setence = "CANT LIST";
           console.error(setence, exception);
   
-          response.status(exception.status | 400);
-          response.json({ mensagem: setence });
+          res.status(exception.status | 404);
+          res.json({ mensagem: setence });
         }
         console.log("LISTED CARDS", cards);
-        response.json(cards);
+        res.json(cards);
       });
   };
 
-api.delete = (request, response) => {
-    neDB.remove({ _id: request.params.id }, {}, (exception, customers) => {
+api.delete = (req, res) => {
+    neDB.remove({ _id: req.params.id }, {}, (exception, customers) => {
     if (exception) {
         const setence = "CANT REMOVE";
         console.error(setence, exception);
 
-        response.status(exception.status | 400);
-        response.json({ mensagem: setence });
+        res.status(exception.status | 404);
+        res.json({ mensagem: setence });
     }
     console.log("CARD REMOVED", customers);
 
-    response.json(customers);
+    res.json(customers);
     });
 };
 
-api.update = (request, response) => {
-    neDB.update({ _id: request.params.id }, {request:request.body.request}, (exception, customers) => {
+api.update = (req, res) => {
+    neDB.update({ _id: req.params.id }, {$set: req.body.$set}, { multi: true }, (exception, customers) => {
         if (exception) {
           const setence = "CANT UPDATE";
           console.error(setence, exception);
   
-          response.status(exception.status | 400);
-          response.json({ mensagem: setence });
+          res.status(exception.status | 404);
+          res.json({ mensagem: setence });
         }
-        console.log("Customer Listado com sucesso", customers);
-        response.json(customers);
+        console.log("SUCCESSFUL UPDATE", customers);
+        res.json(customers);
     });
 };
 
